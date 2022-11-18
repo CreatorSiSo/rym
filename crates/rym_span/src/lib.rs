04c332c90b1c0f065fb4ff4874291d8923cd94a3
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2022-11-18 23:29:39 +0100

    Add annotate_snippets builder abstraction.

--- crates/rym_span/src/lib.rs
+++ crates/rym_span/src/lib.rs
@@ -11,6 +11,23 @@ impl Span {
 	pub const fn new(start: usize, end: usize) -> Self {
 		Self { start, end }
 	}
+
+	pub const fn move_by(self, amount: i32) -> Self {
+		const fn add(lhs: usize, rhs: i32) -> usize {
+			if rhs.is_negative() {
+				lhs - rhs.wrapping_abs() as u32 as usize
+			} else {
+				lhs + rhs as usize
+			}
+		}
+		Self { start: add(self.start, amount), end: add(self.end, amount) }
+	}
+}
+
+impl Into<(usize, usize)> for Span {
+	fn into(self) -> (usize, usize) {
+		(self.start, self.end)
+	}
 }
 
 impl From<&Span> for Range<usize> {
